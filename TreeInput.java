b5a0387af2dd64dcbcae17a5c206750c67a21c27
GOURAV SONI <41364696+gaurav5998soni@users.noreply.github.com>
2023-09-20 10:42:32 +0530

    Fixed tree input code for java

--- TreeInput.java
+++ TreeInput.java
@@ -1,26 +1,37 @@
+package com.coding.challange.tree;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TreeInput {
-    
+
     public static void main(String[] args) {
         System.out.println("Please enter value number of nodes");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
+
         System.out.println("Enter number of edges: ");
         int edges = sc.nextInt();
-        
-        int[] nodes = new int[n];
-        
-        for(int i=0; i<edges; i++){
-            List<Integer> edge = new ArrayList<>();
-            int u = sc.nextInt();
-            nodes[u].add(sc.nextInt());
-            
+
+        List<List<Integer>> nodes = new ArrayList<>();
+
+        for(int i=0; i<=n; i++){
+            nodes.add(new ArrayList<Integer>());
         }
-        
-        
+
+        for(int i=0; i<edges; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            
+            nodes.get(u).add(v);
+            nodes.get(v).add(u);
+        }
+
+        for(int i: nodes.get(1)){
+            System.out.print(i+" ");
+        }
+
+
     }
 }
